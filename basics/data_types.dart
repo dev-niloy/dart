05658100e9b7ab1db53d7a517bdecc103e01bd5f
dev-niloy <niloyroy184@gmail.com>
2024-12-25 20:07:08 +0600

    My first code with dart

---
+++ basics/data_types.dart
@@ -0,0 +1,11 @@
+main(){
+  // integer 1 ,2 >> int
+  // float 1.5, 2.5 >> double
+  // boolean true / false >> bool
+  // text - name, address >> String
+  print(1234);
+  print(25.5);
+  print(true);
+  print(false);
+  print("Hi i am Niloy Roy");
+}
